704c3f38a3a6919238bee0035b495f5ae67cce7f
balraj <kbalrajgoud145@gmail.com>
2020-01-07 22:21:36 +0530

    git

--- 02-print.sh
+++ 02-print.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 ##tonprint something on the screen we use printf command or echo command
 ##echo command we use most
+
 echo hello
+
+## if we want to print mutiple lines
+
+echo hello\nbye
+
+#\n is an escape esquence which prints new line
