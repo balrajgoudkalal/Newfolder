75031d00346cfa51e90f9e2d995f44b34a0dbc21
balraj <kbalrajgoud145@gmail.com>
2020-01-15 21:30:35 +0530

    case

---
+++ 07-conditions.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Two conditional statements are available in bash shell 1.IF 2.CASE
+
+ACTION=$1
+SERVICE_NAME=DEMO
+
+START_FUNCTION(){
+    echo "starting $SERVICE_NAME service"
+
+}
+
+STOP_FUNCTION(){
+    echo "stopiing $SERVICE_NAME service"
+}
+
+case $ACTION in 
+start)
+
+  START_FUNC
+  ;;
+
+stop)
+  STOP_FUNC
+  ;;
+restart)
+  STOP_FUNC
+START_FUNC
+  ;;
+*)
+ echo -e "\n\e[33musage: $0 action(start|stop|restart)"
+ exit 1
+ ;;
+esac 
