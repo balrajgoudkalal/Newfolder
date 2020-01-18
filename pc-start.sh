bd7f1f76f9c2147ab57f1ccc8593cded9e5bb5be
balraj <kbalrajgoud145@gmail.com>
2020-01-18 06:12:28 +0530

    script

--- pc-start.sh
+++ pc-start.sh
@@ -10,12 +10,23 @@ START_F(){
     for service in   $START_ORDER ; do 
        SERVICE=$(echo $service | awk -F : '{print $1}') 
        PORT_NO=$(echo service  | awk -F : '{print $2}')
-       echo "starting $SERVICE service"
+       echo  -n "starting $SERVICE service"
 
        su - $USER_NAME -c  "nohup java -jar /home/$USER_NAME/spring-petclinic-$SERVICE.jar &>/home/$USER_NAME/$SERVICE.log  &"
-       
-       
-
+       local i=0
+       while [ $i -gt $START_TIMEOUT ]; do 
+       netstat -lntp | grep $PORT_no &>/dev/null
+       if [ $? -eq 0 ]; then 
+       echo " -STARTED"
+       break
+       else
+         i= $(($i+15))
+         if [$i -gt $START_TIMEOUT ]; the 
+         echo " - FAILED"
+         exit 1
+         fi 
+       sleep 15
+       fi 
     done
 
     
