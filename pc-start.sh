f273132e2f671a69b9b5894ed264513d00d6c61d
balraj <kbalrajgoud145@gmail.com>
2020-01-18 05:57:16 +0530

    - missing

--- pc-start.sh
+++ pc-start.sh
@@ -3,13 +3,18 @@
 ACTION=$1
 #start function
 USER_NAME=petclinic
-START_ORDER="config-server discovery-server api-gateway vets-service visits-service customers-service"
+START_ORDER="config-server:8888 discovery-server:8761 api-gateway:8080 vets-service:8080 visits-service:8080 customers-service:8080"
 STOP_ORDRER="customers-service  visits-service  vets-service api-gateway discovery-server config-server"
+START_TIMEOUT=300
 START_F(){
     for service in   $START_ORDER ; do 
-       echo "starting $service service"
-       su - $USER_NAME -c  "nohup java -jar /home/$USER_NAME/spring-petclinic-$service.jar &>/home/$USER_NAME/$service.log  &"
-       sleep 1
+       SERVICE=$(echo $service | awk -F : '{print $1}') 
+       PORT_NO=$(echo service  | awk -F : '{print $2}')
+       echo "starting $SERVICE service"
+
+       su - $USER_NAME -c  "nohup java -jar /home/$USER_NAME/spring-petclinic-$SERVICE.jar &>/home/$USER_NAME/$SERVICE.log  &"
+       
+       
 
     done
 
