0d5a83e7d03177b73fa57e91ebc2d3f1882ebe53
balraj <kbalrajgoud145@gmail.com>
2020-01-18 05:48:41 +0530

    while

--- 08-loop.sh
+++ 08-loop.sh
@@ -4,3 +4,18 @@ for var in val1 val2 val3 ; do
  echo $var
  sleep 1
 done
+
+
+## same process can be done by following way 
+
+VALUES="val1 val2 val3"
+ for var in $VALUES ; do
+ echo $var
+sleep 1
+done
+
+i=60 
+
+while [ $i gt 0]; do
+   sleep 2
+   i=$(($i-2))
