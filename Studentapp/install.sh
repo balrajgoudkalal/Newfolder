72a28db6b1d144948c18bef33174306c6506dc6d
balraj <kbalrajgoud145@gmail.com>
2020-01-21 23:05:30 +0530

    print

--- Studentapp/install.sh
+++ Studentapp/install.sh
@@ -42,21 +42,21 @@ if [ $USER_ID -ne 0 ]; then
 fi 
 
 Head "WEB SERVER SETUP"
-Print "install web server\t" # (here -n facilitates success message against the command, we can replace echo -n with print)
+print "install web server\t" # (here -n facilitates success message against the command, we can replace echo -n with print)
 yum install nginx -y &>>$LOG  #(&>>$LOG --if you dont want to see logs )
 STAT_CHECK $?
 
-Print "clean old index files\t"
+print "clean old index files\t"
 rm -rf /usr/share/nginx/html/* &>>$LOG
 STAT_CHECK $?
 
 cd /usr/share/nginx/html/
 
-Print "Download index files\t"
+print "Download index files\t"
 curl -s https://studentapi-cit.s3-us-west-2.amazonaws.com/studentapp-frontend.tar.gz | tar -xz 
 STAT_CHECK $?
 
-Print "Update nginx proxy config"
+print "Update nginx proxy config"
 LINE_NO=$(cat -n /etc/nginx/nginx.conf | grep 'error_page 404' | grep -v '#' |awk '{print $1}')
 sed -i -e "/^#STARTPROXYCONFIG/,/^#STOPPROXYCONFIG/ d" /etc/nginx/nginx.conf 
 sed -i -e "$LINE_NO i #STARTPROXYCONFIG\n\tlocation /student {\n\t\tproxy_pass http://localhost:8080/student;\n\t}\n#STOPPROXYCONFIG" /etc/nginx/nginx.conf
