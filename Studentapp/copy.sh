fc5b0f3be1cc48f999c1159ca1b7f380695fb69a
balraj <kbalrajgoud145@gmail.com>
2020-01-23 06:10:27 +0530

    version changed

--- Studentapp/copy.sh
+++ Studentapp/copy.sh
@@ -14,7 +14,7 @@ G="\e[32m"
 R="\e[31m"
 N="\e[0m"
 FUSERNAME=student
-TOMCAT_VERSION=8.5.47
+TOMCAT_VERSION=8.5.50
 TOMCAT_URL=http://apachemirror.wuchna.com/tomcat/tomcat-8/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
 TOMCAT_HOME=/home/$FUSERNAME/apache-tomcat-${TOMCAT_VERSION}
 
